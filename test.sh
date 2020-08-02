41d466e63bc0f2a88af92fadb1c36cd3845becc7
Samuel Martins <samuel.ubuntu@gmail.com>
2020-08-02 13:19:33 -0300

    Testes com Github Actions

--- test.sh
+++ test.sh
@@ -5,6 +5,6 @@ PGUSER="pguser"
 PGPASS="pgpassword"
 PGDB="sthore_test"
 
-docker exec -i -t pgsql psql postgres -U pguser -c "CREATE DATABASE sthore_test"
+docker exec -i pgsql psql postgres -U pguser -c "CREATE DATABASE sthore_test"
 npm run test:e2e
-docker exec -i -t pgsql psql postgres -U pguser -c "DROP DATABASE sthore_test"
+docker exec -i pgsql psql postgres -U pguser -c "DROP DATABASE sthore_test"
