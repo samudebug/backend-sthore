6bd37a1a89e3c6fad351e34c47f36a6a75153d0b
Samuel Martins <samuel.ubuntu@gmail.com>
2020-08-02 13:21:23 -0300

    Testes com Github Actions

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
 export NODE_ENV="TEST"
 npm run test:cov
-PGUSER="pguser"
-PGPASS="pgpassword"
-PGDB="sthore_test"
+export PGUSER="pguser"
+export PGPASS="pgpassword"
+export PGDB="sthore_test"
 
 docker exec -i pgsql psql postgres -U pguser -c "CREATE DATABASE sthore_test"
 npm run test:e2e
